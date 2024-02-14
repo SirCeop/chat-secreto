f3bdbeeb060d7bc31dedaeb0475c7fdcc51865a4
SirCeop <sir.ceop@gmail.com>
2024-02-14 14:15:29 -0300

    testando novas funções

--- chat_secret.py
+++ chat_secret.py
@@ -9,7 +9,7 @@ import os
 load_dotenv ()
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "token")
+app.secret_key = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
@@ -32,6 +32,11 @@ class Message(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 chat_rooms = {}
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 @app.route('/')
 def index():
@@ -69,7 +74,7 @@ def chat(room_id):
 @app.route('/submit_message/<room_id>', methods=['POST'])
 def submit_message(room_id):
     if room_id not in chat_rooms:
-        return redirect(url_for(''))
+        return redirect(url_for('index'))
 
     if 'username' not in session:
         return redirect(url_for('login'))
@@ -161,4 +166,4 @@ def room():
     return render_template('room.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
